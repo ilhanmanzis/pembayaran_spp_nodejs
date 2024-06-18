4b0fde88b0974db5cea1ed967d5f52a20c8b4fba
ilhanmanzis <115005296+ilhanmanzis@users.noreply.github.com>
2024-06-18 18:43:19 +0700

    Update backend.js

--- src/routers/backend.js
+++ src/routers/backend.js
@@ -1,3 +1,4 @@
+import express from"express";
 import wrapAsync from "../../server.js";
 import auth from "../session/session.js";
 import sessionYes from "../session/sessionYes.js";
@@ -44,92 +45,91 @@ import logout from "./handler/logout/logout.js";
 // admin
 import updateDataAdmin from "./handler/admin/updateDataAdmin.js";
 import updatePassword from "./handler/admin/updatePassword.js";
+const router = express.Router();
 
-const routers = (app)=>{
 
 // data angkatan
     // add data angkatan
-    app.post('/angkatan/create', auth, wrapAsync(addAngkatan));
+    router.post('/angkatan/create', auth, wrapAsync(addAngkatan));
 
     // delete data angkatan
-    app.delete('/angkatan/:id', auth, wrapAsync(deleteAngkatan));
+    router.delete('/angkatan/:id', auth, wrapAsync(deleteAngkatan));
 
     // get data angkatan berdasarkan id
-    app.get('/dataangkatan/:id', auth, wrapAsync(getDataAngkatan));
+    router.get('/dataangkatan/:id', auth, wrapAsync(getDataAngkatan));
 
     // update data angkatan
-    app.put('/editdataangkatan/:id', auth, wrapAsync(updateAngkatan));
+router.put('/editdataangkatan/:id', auth, wrapAsync(updateAngkatan));
 
 
 
 // data jurusan
     // add data jurusan
-    app.post('/jurusan/create', auth, wrapAsync(addJurusan));
+    router.post('/jurusan/create', auth, wrapAsync(addJurusan));
 
     // delete data jurusan
-    app.delete('/jurusan/:id', auth, wrapAsync(deleteJurusan));
+    router.delete('/jurusan/:id', auth, wrapAsync(deleteJurusan));
 
     // update data jurusan
-    app.put('/editdatajurusan/:id', auth, wrapAsync(updateJurusan));
+    router.put('/editdatajurusan/:id', auth, wrapAsync(updateJurusan));
 
 
 
 // data kelas
     // add data kelas
-    app.post('/kelas/create', auth, wrapAsync(addKelas));
+    router.post('/kelas/create', auth, wrapAsync(addKelas));
 
     // delete data kelas
-    app.delete('/kelas/:id', auth, wrapAsync(deleteKelas));
+    router.delete('/kelas/:id', auth, wrapAsync(deleteKelas));
 
     // update data kelas
-    app.put('/editdatakelas/:id', auth, wrapAsync(updateKelas));
+    router.put('/editdatakelas/:id', auth, wrapAsync(updateKelas));
     
 
 // data siswa
     // add data siswa
-    app.post('/siswa', auth, wrapAsync(addSiswa));
+    router.post('/siswa', auth, wrapAsync(addSiswa));
 
     // delete data siswa
-    app.delete('/siswa/:id', auth, wrapAsync(deleteSiswa));
+    router.delete('/siswa/:id', auth, wrapAsync(deleteSiswa));
 
     // update data siswa
-    app.put('/editdatasiswa/:id', auth, wrapAsync(updateSiswa));
+    router.put('/editdatasiswa/:id', auth, wrapAsync(updateSiswa));
 
 
 // data transaksi
     // get data pembayaran
-    app.get('/datapembayaran', auth, wrapAsync(dataPembayaran));
+    router.get('/datapembayaran', auth, wrapAsync(dataPembayaran));
 
     // proses transaksi spp
-    app.get('/prosestransaksi', auth, wrapAsync(prosesTransaksi));
+    router.get('/prosestransaksi', auth, wrapAsync(prosesTransaksi));
 
     // cetak slip transaksi
-    app.get('/cetaksliptransaksi', auth, wrapAsync(cetakSlipTransaksi));
+    router.get('/cetaksliptransaksi', auth, wrapAsync(cetakSlipTransaksi));
 
     // cetak semua transaksi
-    app.get('/cetaksemuatransaksi', auth, wrapAsync(cetakSemuaTransaksi));
+    router.get('/cetaksemuatransaksi', auth, wrapAsync(cetakSemuaTransaksi));
 
 
 // data laporan
     // cetak laporan
-    app.post('/cetaklaporan', auth, wrapAsync(cetakLaporan));
+    router.post('/cetaklaporan', auth, wrapAsync(cetakLaporan));
 
 
 // register
-    app.post('/register', sessionYes, wrapAsync(register));
+    router.post('/register', sessionYes, wrapAsync(register));
 
 // login
-    // app.post('/login', sessionYes, wrapAsync(login));
+     router.post('/login', sessionYes, wrapAsync(login));
 
 // logout
-    app.get('/logout', auth, wrapAsync(logout));
+    router.get('/logout', auth, wrapAsync(logout));
 
 // update data admin
-    app.put('/updatedataadmin',auth, wrapAsync(updateDataAdmin));
+    router.put('/updatedataadmin',auth, wrapAsync(updateDataAdmin));
 
 // update passowrd admin 
-    app.put('/updatepassword', auth, wrapAsync(updatePassword));
+    router.put('/updatepassword', auth, wrapAsync(updatePassword));
 
-}
 
-export default routers;
+export default router;
